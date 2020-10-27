71e5fce1de1295655ede810e0d350e5e1d237156
Vincent Newsom <vnewsom90@gmail.com>
2020-10-27 17:00:44 -0400

    Renders correct answer after question submission Result screen displays how many questions were answered correctly

--- src/screens/QuestionScreen.js
+++ src/screens/QuestionScreen.js
@@ -1,11 +1,13 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { QuestionContext, CountContext } from "../Store";
 import { useHistory, useParams } from "react-router-dom";
 
 export const QuestionScreen = () => {
   let { id } = useParams();
-  const [questions] = useContext(QuestionContext);
+  const [questions, setQuestions] = useContext(QuestionContext);
   const [count, setCount] = useContext(CountContext);
+  const [visible, setVisibility] = useState(false);
+  const [correct, setCorrectness] = useState(false);
   const history = useHistory();
 
   const question = questions[count - 1].question;
@@ -17,7 +19,24 @@ export const QuestionScreen = () => {
     setCount(id * 1);
   }, []);
 
-  const handleOnClick = () => {
+  const handleOnClick = (e) => {
+    let selectedAnswer = e.target.name;
+    if (selectedAnswer == questions[count - 1].correct) {
+      setCorrectness(() => true);
+      setQuestions((questions) =>
+        questions.map((question) =>
+          question.correct === selectedAnswer
+            ? { ...question, gotRight: true }
+            : { ...question }
+        )
+      );
+    }
+    setVisibility((visible) => !visible);
+  };
+
+  const nextQuestionClick = () => {
+    setVisibility((visible) => !visible);
+    setCorrectness(() => false);
     if (count < questions.length) {
       setCount((count) => count + 1);
       history.push("/question/" + (count + 1));
@@ -28,15 +47,43 @@ export const QuestionScreen = () => {
 
   return (
     <div>
-      Question {count}
-      <div>{question}</div>
-      {answers.map((answer, i) => (
+      {!visible && (
         <div>
-          <button onClick={handleOnClick}>
-            {i}. {answer}
-          </button>
+          Question {count}
+          <div>{question}</div>
+          {answers.map((answer, i) => (
+            <div key={answer}>
+              <button name={answer} onClick={handleOnClick}>
+                {i + 1}. {answer}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      )}
+      {visible && correct && (
+        <div className="wrongAnswer">
+          <div style={{ color: "green" }}>Great!!</div>
+          <div style={{ color: "green" }}>
+            <strong style={{ color: "green" }}>
+              {questions[count - 1].correct}{" "}
+            </strong>
+            is the correct answer!
+          </div>
+          <button onClick={nextQuestionClick}>Next question</button>
+        </div>
+      )}{" "}
+      {visible && !correct && (
+        <div className="wrongAnswer">
+          <div>Wrong!</div>
+          <div>
+            Correct answer is{" "}
+            <strong style={{ color: "green" }}>
+              {questions[count - 1].correct}
+            </strong>
+          </div>
+          <button onClick={nextQuestionClick}>Next question</button>
+        </div>
+      )}
     </div>
   );
 };
--- src/screens/ResultsScreen.js
+++ src/screens/ResultsScreen.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { StartButton } from "../components/StartButton";
+import { useContext } from "react";
+import { QuestionContext } from "../Store";
 
 export const ResultsScreen = () => {
+  const [questions] = useContext(QuestionContext);
+  const score = questions.filter((question) => question.gotRight == true)
+    .length;
   return (
     <div>
-      These are your results!
-      <div>Thank you for playing!</div>
+      {score === 10 ? (
+        <div>Perfect score!</div>
+      ) : score > 7 ? (
+        <div>Keep it up almost there</div>
+      ) : (
+        <div>Better Luck Next time</div>
+      )}
+      <div>
+        {score} out of {questions.length}{" "}
+      </div>
+
       <StartButton message={"Play Again?"} />
     </div>
   );
