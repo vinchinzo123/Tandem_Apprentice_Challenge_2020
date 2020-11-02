e78539c477e8aaf4538ff45872652d26701754a1
Vincent Newsom <vnewsom90@gmail.com>
2020-11-01 19:08:24 -0500

    Saves time from timer to local state

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,18 +1,33 @@
 import React, { useEffect, useState } from "react";
 
 export const Timer = () => {
-  const [time, setTime] = useState(30);
+  const [time, setTime] = useState(() => {
+    try {
+      if (localStorage.getItem("time") !== null) {
+        return Number.parseInt(JSON.parse(localStorage.getItem("time")), 10);
+      } else {
+        return 30;
+      }
+    } catch {
+      return 30;
+    }
+  });
 
   useEffect(() => {
     const startTimer = () => {
       setTime((time) => {
         if (time > 0) {
+          localStorage.setItem("time", time - 1);
           return time - 1;
         }
+        return 0;
       });
     };
     let tID = setInterval(() => startTimer(), 1000);
-    return () => clearInterval(tID);
+    return () => {
+      localStorage.removeItem("time");
+      clearInterval(tID);
+    };
     // eslint-disable-next-line
   }, []);
 
